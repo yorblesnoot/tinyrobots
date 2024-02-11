b879cdaf16c8e648953d0737530c475899b88112
yorblesnoot <ethereos@gmail.com>
2024-02-11 00:58:12 -0500

    rig for laser turret

--- Assets/Scripts/Movement/PropellerFly.cs
+++ Assets/Scripts/Movement/PropellerFly.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PropellerMove : PrimaryMovement
+public class PropellerFly : PrimaryMovement
 {
     private void Awake()
     {
