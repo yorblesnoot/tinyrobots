36502e7e2f531c89097aa56605e00c5dd28c1c9f
yorblesnoot <ethereos@gmail.com>
2024-07-22 15:47:41 -0400

    added railgun model

--- Assets/Scripts/Battle/TinyBot/PartSystem/PartModifier.cs
+++ Assets/Scripts/Battle/TinyBot/PartSystem/PartModifier.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 public class PartModifier : MonoBehaviour
 {
     public List<Renderer> mainRenderers;
-    public Ability[] Abilities;
+    [SerializeField] Transform abilityContainer;
+    [HideInInspector] public Ability[] Abilities;
+
+    private void Awake()
+    {
+        Abilities = abilityContainer.GetComponentsInChildren<Ability>();
+    }
 }
