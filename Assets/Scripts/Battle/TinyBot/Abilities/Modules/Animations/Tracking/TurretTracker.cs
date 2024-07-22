59560abc45d07f85794383eab7c4c27652f01f0a
yorblesnoot <ethereos@gmail.com>
2024-07-22 19:10:21 -0400

    added railgun

--- Assets/Scripts/Battle/TinyBot/Abilities/Modules/Animations/Tracking/TurretTracker.cs
+++ Assets/Scripts/Battle/TinyBot/Abilities/Modules/Animations/Tracking/TurretTracker.cs
@@ -7,9 +7,7 @@ public class TurretTracker : TrackingAnimation
 
     public override void Aim(List<Vector3> trajectory)
     {
-        Vector3 localizedTarget = trajectory[^1];
-        localizedTarget = transform.InverseTransformPoint(localizedTarget);
-        ikTarget.localPosition = Vector3.Lerp(ikTarget.localPosition, localizedTarget, 1/slowness);
+        ikTarget.position = Vector3.Lerp(ikTarget.position, trajectory[^1], 1/slowness);
     }
 
 }
